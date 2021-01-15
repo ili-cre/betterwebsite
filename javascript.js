62fb334e0e1b0d566d308676b5db0dad35bb41d8
Ilinca Cretu <ilinca.cretu@yahoo.com>
2021-01-15 11:49:54 +0100

    innit

---
+++ javascript.js
@@ -0,0 +1,12 @@
+function sendFeedback(event) 
+{
+    event.preventDefault();
+
+var formData = event.srcElement;
+
+    document.getElementById("thankYou").innerText = "Hey " + formData.name.value + "!" + " It seems you are also a food lover, like me! Keep an eye on your inbox for my reply!" ;
+
+    console.log(formData.name.value);
+    console.log(formData.email.value);
+    console.log(formData.message.value);
+}
